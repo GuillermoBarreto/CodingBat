2e6ec1955c3a8667d41aeb6c364a76123fb7948b
GuillermoBarreto <Guilleelias25@gmail.com>
2025-04-04 22:00:15 -0400

    exercise

---
+++ front3.java
@@ -0,0 +1,14 @@
+//Given a string, we'll say that the front is the first 3 chars of the string.
+// if the string length is less than 3, the front is whatever is there. 
+//return a new string which is 3 copies of the front. 
+
+//front3("java")-> "JavJavJav"
+//front3("Chocolate")-> "ChoChoCho"
+//front3("abc") -> "abcabcabc"
+
+
+public String front3(String str) {
+    if (str.length() < 3) {
+        
+    }
+}
