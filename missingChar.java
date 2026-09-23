c43c88a527c109f8292885c893df9ef72555a951
Guillermo Barreto <124272218+GuillermoBarreto@users.noreply.github.com>
2026-09-22 20:23:51 -0500

    Simplify tail substring in missingChar

--- missingChar.java
+++ missingChar.java
@@ -11,7 +11,7 @@ public String missingChar(String str, int n) {
     // Start this substring at n+1 to omit the char.
     // Can also be shortened to just str.substring(n+1)
     // which goes through the end of the string.
-    String back = str.substring(n+1, str.length());
+    String back = str.substring(n+1);
     
     return front + back;
   }
